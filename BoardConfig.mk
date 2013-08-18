1bb54102efcd0e1139618238d8fbf43bc4525bda
poonchitim <poonchitim@yahoo.com>
2013-08-18 17:43:42 +0800

    device: p880: update for mokee 4.3
    Change-Id: I8386740b6d73cc0fcc712e40c4c87d5d2956dc7d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,20 +8,21 @@ TARGET_BOOTLOADER_BOARD_NAME := p880
 
 # Target arch settings
 TARGET_NO_BOOTLOADER := true
+TARGET_ARCH := arm
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_ARCH_VARIANT_CPU := cortex-a9
+TARGET_CPU_VARIANT := cortex-a9
+TARGET_ARCH_VARIANT_CPU := $(TARGET_CPU_VARIANT)
 TARGET_CPU_SMP := true
 ARCH_ARM_HAVE_TLS_REGISTER := true
 ARCH_ARM_USE_NON_NEON_MEMCPY := true
-TARGET_ARCH := arm
 
 BOARD_KERNEL_CMDLINE := 
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
 
-# fix this up by examining /proc/mtd on a running device
+# Block Size
 BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1056859750
@@ -31,21 +32,18 @@ BOARD_HAS_LARGE_FILESYSTEM := true
 
 TARGET_RECOVERY_PRE_COMMAND := "/system/bin/setup-recovery"
 
-# Try to build the kernel
+# Kernel
 TARGET_KERNEL_CONFIG := mokee_x3_defconfig
-# Keep this as a fallback
 TARGET_PREBUILT_KERNEL := device/lge/p880/kernel
-
-BOARD_HAS_NO_SELECT_BUTTON := true
-#BOARD_TOUCH_RECOVERY := true
-TARGET_USERIMAGES_USE_EXT4 := true
 
 TARGET_SPECIFIC_HEADER_PATH := device/lge/p880/include
 
+#FB
 BOARD_NO_ALLOW_DEQUEUE_CURRENT_BUFFER := true
 BOARD_USE_SKIA_LCDTEXT := true
 BOARD_EGL_CFG := device/lge/p880/egl.cfg
 USE_OPENGL_RENDERER := true
+BOARD_EGL_NEEDS_LEGACY_FB := true
 
 # Wifi related defines
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
@@ -60,24 +58,38 @@ WIFI_DRIVER_FW_PATH_P2P := "/system/etc/firmware/fw_bcmdhd_p2p.bin"
 WIFI_DRIVER_FW_PATH_AP := "/system/etc/firmware/fw_bcmdhd_apsta.bin"
 BOARD_LEGACY_NL80211_STA_EVENTS := true
 
+# Bootnimation
 TARGET_BOOTANIMATION_PRELOAD := true
 TARGET_BOOTANIMATION_TEXTURE_CACHE := true
 
+# BT
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
 BOARD_BLUEDROID_VENDOR_CONF := device/lge/p880/bluetooth/vnd_bt.txt
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/lge/p880/bluetooth
 
+# Hardware
+BOARD_HAS_NO_SELECT_BUTTON := true
+
+TARGET_USERIMAGES_USE_EXT4 := true
+
 COMMON_GLOBAL_CFLAGS += -DMR0_AUDIO_BLOB -DMR0_CAMERA_BLOB
-BOARD_EGL_NEEDS_LEGACY_FB := true
+
+BOARD_HARDWARE_CLASS := device/lge/p880/cmhw/
 
 ## Radio fixes
 BOARD_RIL_CLASS := ../../../device/lge/p880/ril/
 
+# Recovery
+TARGET_RECOVERY_FSTAB := device/lge/p880/fstab.x3
+RECOVERY_FSTAB_VERSION := 2
 BOARD_CUSTOM_GRAPHICS := ../../../device/lge/p880/recovery-gfx.c
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/lge/p880/recovery-keys.c
+
+# Changer
 BOARD_CHARGER_ENABLE_SUSPEND := true
 
+# SeLinux
 ifeq ($(HAVE_SELINUX),true)
 
 BOARD_SEPOLICY_DIRS := \
@@ -89,8 +101,4 @@ BOARD_SEPOLICY_UNION := \
     device.te \
     domain.te
 
-endif
-
-ifdef MK_RELEASE
-WITH_DEXPREOPT := true
 endif
